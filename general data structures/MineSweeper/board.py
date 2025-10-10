575273ab0c0b018f0d5f420184b23272bb167123
aiden100a <235563118+brucea-school@users.noreply.github.com>
2025-10-10 13:28:04 -0400

    f**k it we ball

--- general data structures/MineSweeper/board.py
+++ general data structures/MineSweeper/board.py
@@ -2,6 +2,8 @@ import math
 import os
 import random
 import sys
+
+# Set high recursion limit for deep recursive digs of safe spaces
 sys.setrecursionlimit(99999999)
 
 # colors
@@ -29,36 +31,47 @@ CYAN_BG = '\033[46m'
 WHITE_BG = '\033[47m'
 
 RESET = '\033[0m'
+
+
 class Board:
 
     def __init__(self, x: int, mines: int):
-        self.mineBoard = [' ']*x
+        self.mineBoard = [' '] * x
         self.startingMineAmt = mines
-        self.flags = 0
+        # self.flags is now removed as flags are calculated dynamically
         self.status = "run"
 
         for i in range(0, len(self.mineBoard)):
-            self.mineBoard[i] = [False]*x
+            self.mineBoard[i] = [False] * x
 
         i = 0
 
-        while i <= self.startingMineAmt:
-            ex = random.randint(0, len(self.mineBoard)-1)
-            ey = random.randint(0, len(self.mineBoard[0])-1)
-            if not self.isMine(ex,ey):
-                #print("("+str(ex)+","+str(ey)+")")
+        # Place mines randomly
+        while i < self.startingMineAmt:
+            ex = random.randint(0, len(self.mineBoard) - 1)
+            ey = random.randint(0, len(self.mineBoard[0]) - 1)
+            if not self.isMine(ex, ey):
                 self.mineBoard[ex][ey] = True
-                i+=1
+                i += 1
 
-        # make payer board
-        self.playerBoard = [' ']*x
+        # make player board
+        self.playerBoard = [' '] * x
 
         for i in range(0, len(self.playerBoard)):
-            self.playerBoard[i] = [' ']*x
+            self.playerBoard[i] = [' '] * x
+
+    def count_flags(self) -> int:
+        """Counts the total number of flags currently placed on the player board."""
+        count = 0
+        for r in range(len(self.playerBoard)):
+            for c in range(len(self.playerBoard[0])):
+                if self.playerBoard[r][c] == 'F':
+                    count += 1
+        return count
 
     def printBoard(self):
 
-        #print face
+        # print face
         if (self.isGameOver() == "run"):
             print("🙂")
         elif (self.isGameOver() == "loss"):
@@ -66,209 +79,290 @@ class Board:
         else:
             print("😎")
 
-        minesLeft = str(self.startingMineAmt - self.flags)
+        current_flags = self.count_flags()
+        minesLeft = str(self.startingMineAmt - current_flags)
 
+        # Mine counter display
         if len(minesLeft) == 1:
-            print(BLACK_BG+RED_TEXT+"00"+minesLeft[0]+RESET)
+            print(BLACK_BG + RED_TEXT + "00" + minesLeft[0] + RESET)
         elif len(minesLeft) == 2:
-            print(BLACK_BG + RED_TEXT + "0"+ minesLeft[1] + minesLeft[0] + RESET)
+            print(BLACK_BG + RED_TEXT + "0" + minesLeft[0] + minesLeft[1] + RESET)  # Fixed indexing here
         else:
             print(BLACK_BG + RED_TEXT + minesLeft + RESET)
         print()
 
-        #print numbers
-        if len(str(len(self.playerBoard))) == 2:
-            print("  ",end="")
-        else:
-            print(" ",end="")
+        # print numbers for columns
+        size = len(self.playerBoard)
 
+        # Determine prefix spacing for rows
+        prefix_len = len(str(size - 1))
+        prefix = " " * prefix_len
+        print(prefix + " ", end="")
 
-        if len(str(len(self.playerBoard))) >1:
-            for i in range(0, len(self.playerBoard)):
-                if len(str(i)) == 1:
-                    print(" ",end="")
-                else:
-                    print(str(i)[0],end="")
+        # Print column coordinate part 1 (tens digit)
+        if size > 10:
+            for i in range(size):
+                print(str(i).zfill(2)[0], end="")
             print()
-            if len(str(len(self.playerBoard))) == 2:
-                print("  ", end="")
-            else:
-                print(" ", end="")
-            for i in range(0, len(self.playerBoard)):
-                if len(str(i)) == 1:
-                    print(i, end="")
-                else:
-                    print(str(i)[1], end="")
-        else:
-            for i in range(0, len(self.playerBoard)):
-                if len(str(i)) == 1:
-                    print(i, end="")
-                else:
-                    print(str(i)[1], end="")
+            print(prefix + " ", end="")
 
-
+        # Print column coordinate part 2 (ones digit)
+        for i in range(size):
+            print(str(i).zfill(2)[-1], end="")
         print()
-        b = 0
-        for x in range(0, len(self.playerBoard)):
 
-            if len(self.playerBoard[0]) > 10:
-                if b < 10:
-                    print(" "+str(b), end='')
-                else:
-                    print( str(b), end='')
-            else:
-                print(b, end='')
+        # Print the board content
+        for y in range(size):
+            # Print row coordinate (y-axis)
+            print(str(y).zfill(prefix_len), end=' ')
 
-            b += 1
-            for h in range(0, len(self.playerBoard)):
-                #print actual board
-                y = self.playerBoard[h][x]
-                if y == " ":
-                 print(GRAY_BG+str(y)+RESET , end='')
-                elif y == "-":
+            for x in range(size):
+                # print actual board
+                cell = self.playerBoard[x][y]
+                if cell == " ":
+                    print(GRAY_BG + " " + RESET, end='')
+                elif cell == "-":
                     print(BLACK_BG + " " + RESET, end='')
-                elif y == "F":
-                    print(RED_TEXT+ GRAY_BG + "⚑" + RESET, end='')
-                elif str(y) == "1":
+                elif cell == "F":
+                    # Use a flag symbol
+                    print(RED_TEXT + GRAY_BG + "⚑" + RESET, end='')
+                elif str(cell) == "1":
                     print(BLACK_BG + BLUE_TEXT + "1" + RESET, end='')
-                elif str(y) == "2":
+                elif str(cell) == "2":
                     print(BLACK_BG + GREEN_TEXT + "2" + RESET, end='')
-                elif str(y) == "3":
+                elif str(cell) == "3":
                     print(BLACK_BG + RED_TEXT + "3" + RESET, end='')
-                elif str(y) == "4":
+                elif str(cell) == "4":
                     print(BLACK_BG + MAGENTA_TEXT + "4" + RESET, end='')
-                elif str(y) == "5":
+                elif str(cell) == "5":
                     print(BLACK_BG + RED_TEXT + BOLD + "5" + RESET, end='')
-                elif str(y) == "6":
+                elif str(cell) == "6":
                     print(BLACK_BG + CYAN_TEXT + "6" + RESET, end='')
-                elif str(y) == "7":
+                elif str(cell) == "7":
                     print(BLACK_BG + GRAY_TEXT + "7" + RESET, end='')
-                elif str(y) == "8":
+                elif str(cell) == "8":
                     print(BLACK_BG + GRAY_TEXT + BOLD + "8" + RESET, end='')
-                elif str(y) == "M":
+                elif str(cell) == "M":
                     print(RED_BG + WHITE_TEXT + BOLD + "!" + RESET, end='')
-                elif str(y) == "?":
+                elif str(cell) == "?":
                     print(YELLOW_BG + BLACK_TEXT + BOLD + BLINK + "#" + RESET, end='')
-            print(b-1)
 
-        if len(str(len(self.playerBoard))) == 2:
-            print("  ", end="")
-        else:
-            print(" ", end="")
+            # Print row coordinate again on the right
+            print(str(y).zfill(prefix_len))
 
-        if len(str(len(self.playerBoard))) > 1:
-            for i in range(0, len(self.playerBoard)):
-                if len(str(i)) == 1:
-                    print(" ", end="")
-                else:
-                    print(str(i)[0], end="")
+        # Print column coordinates again at the bottom
+        print(prefix + " ", end="")
+        if size > 10:
+            for i in range(size):
+                print(str(i).zfill(2)[0], end="")
             print()
-            if len(str(len(self.playerBoard))) == 2:
-                print("  ", end="")
-            else:
-                print(" ", end="")
-            for i in range(0, len(self.playerBoard)):
-                if len(str(i)) == 1:
-                    print(i, end="")
-                else:
-                    print(str(i)[1], end="")
-        else:
+            print(prefix + " ", end="")
 
-            for i in range(0, len(self.playerBoard)):
-                if len(str(i)) == 1:
-                    print(i, end="")
-                else:
-                    print(str(i)[1], end="")
-
+        for i in range(size):
+            print(str(i).zfill(2)[-1], end="")
         print()
 
-
-
-    def isMine(self,x:int,y:int):
-        #is out of range?
-        if x < 0:
+    def isMine(self, x: int, y: int):
+        """Checks if a coordinate is within bounds and contains a mine."""
+        if x < 0 or x >= len(self.mineBoard):
             return False
-        if x >= len(self.mineBoard):
+        if y < 0 or y >= len(self.mineBoard[0]):
             return False
-        if y < 0:
-            return False
-        if y >= len(self.mineBoard[0]):
-            return False
-        # check mine
         return self.mineBoard[x][y]
 
-    def tryToDig(self, x:int,y:int):
+    def tryToDig(self, x: int, y: int):
+        """
+        Attempts to dig a cell. Handles game state changes and recursive
+        expansion for safe '-' cells.
+        """
         # is out of range?
-        if x < 0:
-            return
-        if x > len(self.mineBoard)-1:
-            return
-        if y < 0:
-            return
-        if y > len(self.mineBoard[0])-1:
+        if x < 0 or x >= len(self.mineBoard) or y < 0 or y >= len(self.mineBoard[0]):
             return
 
-        # are you checked alerdy?
+        # are you checked already or flagged?
         if not (self.playerBoard[x][y] == ' ' or self.playerBoard[x][y] == 'F'):
             return
 
+        # If flagged, do not dig (AI should not attempt this, but safety check)
+        if self.playerBoard[x][y] == 'F':
+            return
 
-        if self.isMine(x,y):
+        if self.isMine(x, y):
             # KABOOM!
-            self.status="loss"
+            self.status = "loss"
             self.playerBoard[x][y] = "M"
         else:
-            #scan for mines
-            self.playerBoard[x][y] = "?"
+            # scan for mines in neighbors
+            self.playerBoard[x][y] = "?"  # Temporary set to prevent infinite recursion
             sourrond = 0
-            if self.isMine(x-1,y-1):
-                sourrond += 1
-            if self.isMine(x,y-1):
-                sourrond += 1
-            if self.isMine(x-1,y):
-                sourrond += 1
-            if self.isMine(x+1,y-1):
-                sourrond += 1
-            if self.isMine(x-1,y+1):
-                sourrond += 1
-            if self.isMine(x+1,y+1):
-                sourrond += 1
-            if self.isMine(x,y+1):
-                sourrond += 1
-            if self.isMine(x+1,y):
-                sourrond += 1
 
-
+            # Iterate through all 8 neighbors
+            for dx in [-1, 0, 1]:
+                for dy in [-1, 0, 1]:
+                    if dx == 0 and dy == 0:
+                        continue
+                    if self.isMine(x + dx, y + dy):
+                        sourrond += 1
 
             if sourrond == 0:
-                #if you are a blank space
+                # if you are a blank space ('-'), recursively dig neighbors
                 self.playerBoard[x][y] = "-"
-                self.tryToDig(x + 1,y)
-                self.tryToDig(x - 1, y)
-                self.tryToDig(x, y+1)
-                self.tryToDig(x, y - 1)
-                self.tryToDig(x + 1, y-1)
-                self.tryToDig(x - 1, y + 1)
-                self.tryToDig(x - 1, y - 1)
-                self.tryToDig(x + 1, y + 1)
-                return
+                # Recursive dig on all 8 neighbors
+                for dx in [-1, 0, 1]:
+                    for dy in [-1, 0, 1]:
+                        if dx == 0 and dy == 0:
+                            continue
+                        self.tryToDig(x + dx, y + dy)
             else:
-                #add number
+                # add number
                 self.playerBoard[x][y] = str(sourrond)
 
-
-
-
-
-
     def isGameOver(self) -> str:
-        #check for win
+        """Checks for win (all non-mine cells are dug) or loss."""
+        if self.status == "loss":
+            return "loss"
+
+        # check for win
+        # The game is won if every non-mine cell is revealed.
         for x in range(0, len(self.mineBoard)):
             for y in range(0, len(self.mineBoard[0])):
-                if self.playerBoard[x][y] == 'F' or self.playerBoard[x][y] == ' ':
-                    if not( self.isMine(x,y)):
-                        #just retun the current status
-                        return  self.status
+                is_mine = self.isMine(x, y)
+                # is_revealed means the cell is not ' ' (unscanned) or 'F' (flagged)
+                is_revealed = self.playerBoard[x][y] not in (' ', 'F')
 
+                if not is_mine and not is_revealed:
+                    # Found an unrevealed safe spot, game is still running
+                    return self.status
+
+        # If we reached here, all non-mine cells are revealed (or only flags remain on mine spots)
         return "win"
 
+    def get_neighbors(self, x: int, y: int) -> list:
+        """Returns a list of valid neighbor (nx, ny) coordinates."""
+        neighbors = []
+        size = len(self.playerBoard)
+        for dx in [-1, 0, 1]:
+            for dy in [-1, 0, 1]:
+                if dx == 0 and dy == 0:
+                    continue
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < size and 0 <= ny < size:
+                    neighbors.append((nx, ny))
+        return neighbors
+
+    def get_neighbor_status(self, x: int, y: int) -> tuple:
+        """
+        Analyzes neighbor status for a cell.
+        Returns: (flagged_count, unopened_count, unopened_coords)
+        """
+        neighbors = self.get_neighbors(x, y)
+        flagged = 0
+        unopened = 0
+        unopened_coords = []
+
+        for nx, ny in neighbors:
+            cell = self.playerBoard[nx][ny]
+            if cell == 'F':
+                flagged += 1
+            # Unopened means ' ' (unscanned)
+            elif cell == ' ':
+                unopened += 1
+                unopened_coords.append((nx, ny))
+
+        return flagged, unopened, unopened_coords
+
+    def ai_move(self) -> tuple[bool, str]:
+        """
+        The core AI logic. Executes one deterministic move or makes a random guess.
+        Returns: (was_move_made, status_message)
+        """
+        size = len(self.playerBoard)
+
+        # 1. First move (Random dig if the board is completely empty)
+        is_empty = all(self.playerBoard[r][c] == ' ' for r in range(size) for c in range(size))
+        if is_empty:
+            rx, ry = random.randint(0, size - 1), random.randint(0, size - 1)
+            self.tryToDig(rx, ry)
+
+            if self.status == "loss":
+                return False, "AI: Lost on first random dig at ({},{})".format(rx, ry)
+
+            return True, "AI: Initial random dig at ({},{})".format(rx, ry)
+
+        # 2. Apply deterministic logic (Rule 1 & Rule 2)
+        # We store moves to make sure we only make one move per call to avoid infinite recursion/loops.
+        moves_to_make = []
+
+        for r in range(size):
+            for c in range(size):
+                cell = self.playerBoard[r][c]
+
+                # Only analyze cells that contain a number
+                if cell.isdigit():
+                    mine_count = int(cell)
+                    flagged, unopened, unopened_coords = self.get_neighbor_status(r, c)
+
+                    # Rule 1: Guaranteed Mine (Flagging)
+                    mines_remaining_to_find = mine_count - flagged
+                    if mines_remaining_to_find > 0 and mines_remaining_to_find == unopened:
+                        # Add flagging moves
+                        for nx, ny in unopened_coords:
+                            moves_to_make.append(('flag', nx, ny))
+                        # Only execute the first move found and return
+                        if moves_to_make:
+                            break
+
+                    # Rule 2: Guaranteed Safe Dig (Saturated)
+                    # If all surrounding mines are already flagged (flagged == N),
+                    # all remaining unopened cells must be safe.
+                    elif flagged == mine_count and unopened > 0:
+                        # Add digging moves
+                        for nx, ny in unopened_coords:
+                            moves_to_make.append(('dig', nx, ny))
+                        # Only execute the first move found and return
+                        if moves_to_make:
+                            break
+            if moves_to_make:
+                break
+
+        # Execute the first deterministic move found, if any
+        if moves_to_make:
+            action, x, y = moves_to_make[0]
+
+            if action == 'flag':
+                # Check if it's already flagged by another rule application in this turn (unlikely but safe)
+                if self.playerBoard[x][y] == ' ':
+                    self.playerBoard[x][y] = 'F'
+                    return True, "AI: Flagged ({},{}) deterministically (Rule 1)".format(x, y)
+
+            elif action == 'dig':
+                # Digging might trigger recursive tries, which is fine
+                if self.playerBoard[x][y] == ' ':
+                    self.tryToDig(x, y)
+
+                    # Check if we lost after the dig
+                    if self.status == "loss":
+                        return False, "AI: Lost by digging a mine at ({},{}) (Rule 2 failure)".format(x, y)
+
+                    return True, "AI: Dug ({},{}) deterministically (Rule 2)".format(x, y)
+
+        # 3. No deterministic move found: Make a random guess (as requested)
+        unopened_safe_cells = []
+        for r in range(size):
+            for c in range(size):
+                if self.playerBoard[r][c] == ' ':  # Only look for unopened, unflagged spots
+                    unopened_safe_cells.append((r, c))
+
+        if unopened_safe_cells:
+            # Make a random guess from available unopened cells
+            rx, ry = random.choice(unopened_safe_cells)
+            self.tryToDig(rx, ry)
+
+            # Check if we lost immediately after the guess
+            if self.status == "loss":
+                return False, "AI: Lost by guessing a mine at ({},{})".format(rx, ry)
+
+            return True, "AI: Guessed randomly at ({},{})".format(rx, ry)
+
+        # 4. If no moves were made (deterministic or guess), it means the board is solved (all non-mines are revealed)
+        return False, "AI: No moves left to make (Board solved or stuck)."
--- general data structures/MineSweeper/main.py
+++ general data structures/MineSweeper/main.py
@@ -3,24 +3,36 @@ I RECOMMEND CHANGING YOUR TERMINAL FONT HEIGHT TO 0.6 FOR BETTER READABILITY
 """
 
 from board import *
+import time # Import time for AI delay
 
 print(RED_BG+"HEY!"+RESET)
 print(RED_TEXT+"I RECOMMEND CHANGING YOUR TERMINAL FONT HEIGHT TO 0.6 FOR BETTER READABILITY")
 print("see Settings > Editor > Console Scheme > Console Font"+RESET)
 print("\n"*3)
 
-print("input board size  "+GRAY_TEXT+"(must be more than 10 and less than 100):"+RESET)
-size = int( input())
-print("input board mines#:")
-mines = int(input())
+print("input board size " + GRAY_TEXT + "(must be more than 10 and less than 100):" + RESET)
+try:
+    size = int(input())
+except ValueError:
+    print(RED_BG + "Invalid input. Using default size 10." + RESET)
+    size = 10
 
-brd = Board(size,mines)
+print("input board mines#:")
+try:
+    mines = int(input())
+except ValueError:
+    print(RED_BG + "Invalid input. Using default mines 15." + RESET)
+    mines = 15
+
+# Ensure valid size/mine counts
+size = max(10, min(100, size))
+mines = max(1, min(size * size - 10, mines))
+
+brd = Board(size, mines)
 
 lastRoundStatus = ""
 
 while brd.isGameOver() == "run":
-
-
 
     print("\n"*50)
 
@@ -31,23 +43,22 @@ while brd.isGameOver() == "run":
         lastRoundStatus = ""
     else:
         print()
-    print("what do you want to do?:     "+GRAY_TEXT+"(you can type "+BOLD+"help"+RESET+GRAY_TEXT+" for help)"+RESET)
+
+    print("what do you want to do?:     "+GRAY_TEXT+"(you can type "+BOLD+"help"+RESET+GRAY_TEXT+" or "+BOLD+"AI"+RESET+GRAY_TEXT+" for automatic play)"+RESET)
     intake = input()
 
-    #inkae commands
+    # intake commands
 
     if intake.startswith("d"):
-        xwant = int(input("x cord?:"))
-        ywant = int(input("y cord?:"))
+        try:
+            xwant = int(input("x cord?:"))
+            ywant = int(input("y cord?:"))
+        except ValueError:
+            lastRoundStatus = "Invalid coordinate input!"
+            continue
 
-
-        if xwant < 0:
-            lastRoundStatus = "OUT OF RANGE"
-        elif xwant >= len(brd.mineBoard):
-            lastRoundStatus = "OUT OF RANGE"
-        elif ywant < 0:
-            lastRoundStatus = "OUT OF RANGE"
-        elif ywant >= len(brd.mineBoard):
+        board_size = len(brd.mineBoard)
+        if xwant < 0 or xwant >= board_size or ywant < 0 or ywant >= board_size:
             lastRoundStatus = "OUT OF RANGE"
         elif brd.playerBoard[xwant][ywant] == "F":
             lastRoundStatus = BOLD+"OOPS!"+RESET+RED_BG+" looks like ("+str(xwant)+","+str(ywant)+") is flagged! command ingnored"
@@ -59,24 +70,25 @@ while brd.isGameOver() == "run":
                 brd.playerBoard[xwant][ywant] = "?"
                 brd.printBoard()
                 brd.playerBoard[xwant][ywant] = " "
-                print("are you sure you want to dig there?"+GRAY_TEXT+"  type y to dig"+RESET)
+                print("are you sure you want to dig there?"+GRAY_TEXT+"  type y to dig"+RESET)
                 if input() == "y":
                     brd.tryToDig(xwant,ywant)
                 else:
                     lastRoundStatus = RESET+MAGENTA_BG+"dig aborted!"+RESET
         else:
             lastRoundStatus = "CANT DIG THERE!"
-    elif intake.startswith("f"):
-        xwant = int(input("x cord?:"))
-        ywant = int(input("y cord?:"))
 
-        if xwant < 0:
-            lastRoundStatus = "OUT OF RANGE"
-        elif xwant >= len(brd.mineBoard)-1:
-            lastRoundStatus = "OUT OF RANGE"
-        elif ywant < 0:
-            lastRoundStatus = "OUT OF RANGE"
-        elif ywant >= len(brd.mineBoard)-1:
+    elif intake.startswith("f"):
+        try:
+            xwant = int(input("x cord?:"))
+            ywant = int(input("y cord?:"))
+        except ValueError:
+            lastRoundStatus = "Invalid coordinate input!"
+            continue
+
+        board_size = len(brd.mineBoard)
+        # Corrected range check: max index is board_size - 1
+        if xwant < 0 or xwant >= board_size or ywant < 0 or ywant >= board_size:
             lastRoundStatus = "OUT OF RANGE"
         elif brd.playerBoard[xwant][ywant] == "F":
             brd.playerBoard[xwant][ywant] = " "
@@ -86,6 +98,32 @@ while brd.isGameOver() == "run":
             lastRoundStatus = RESET + BLACK_TEXT + YELLOW_BG + "Flag added" + RESET
         else:
             lastRoundStatus = "UNABLE TO FLAG THERE!"
+
+    # --- AI Command Implementation ---
+    elif intake.startswith("AI"):
+        moved = True
+
+        print("\n"*50)
+        print(CYAN_BG + BLACK_TEXT + "AI playing... (Press Ctrl+C to stop)" + RESET)
+
+        # Loop until no deterministic move is found or the game ends
+        while moved and brd.isGameOver() == "run":
+            moved, status_msg = brd.ai_move()
+
+            if moved:
+                # Clear screen and show board after each move
+                print("\n"*50)
+                brd.printBoard()
+                print(CYAN_BG + BLACK_TEXT + status_msg + RESET)
+                time.sleep(0.1) # Pause to see the move
+
+        # After the loop finishes (either no moves or game over)
+        if brd.isGameOver() != "run":
+            lastRoundStatus = GREEN_BG + BLACK_TEXT + "AI finished the game! Status: " + brd.isGameOver().upper() + RESET
+        else:
+            lastRoundStatus = YELLOW_BG + BLACK_TEXT + "AI stopped: No more deterministic moves found. Manual intervention required." + RESET
+    # --- End AI Command ---
+
     elif intake.startswith("e"):
         print("\n"*100)
         print("GOOD BYE!!!")
@@ -93,19 +131,19 @@ while brd.isGameOver() == "run":
 
     elif intake.startswith("help"):
         print("\n"*100)
-        print(CYAN_TEXT+"💣MINESWEEPER💣"+ RESET + GRAY_TEXT + "           if you dont know how to play: look it up")
+        print(CYAN_TEXT+"💣MINESWEEPER💣"+ RESET + GRAY_TEXT + "           if you dont know how to play: look it up")
         print("")
         print(GREEN_TEXT+"COMMANDS"+RESET)
         print(BLUE_TEXT+"d - dig"+RESET)
         print(CYAN_TEXT + "dc - dig without confirm"+RESET)
-        print(YELLOW_TEXT+"f - add a flag"+RESET)
+        print(YELLOW_TEXT+"f - add/remove a flag"+RESET)
+        print(MAGENTA_TEXT+"AI - let the computer play deterministically"+RESET)
         print(RED_TEXT+"e - leave"+RESET)
         input("press enter to continue")
 
     else:
         lastRoundStatus = "COMMAND DOSE NOT EXIST!"
+
+# Final status display
 brd.printBoard()
 print("you "+brd.isGameOver())
-
-
-
