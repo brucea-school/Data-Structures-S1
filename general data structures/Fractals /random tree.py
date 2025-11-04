eff0de04211b9882e350feb6d127f9c059aa902c
aiden100a <235563118+brucea-school@users.noreply.github.com>
2025-11-04 12:31:04 -0500

    biblically accurate tree

--- general data structures/Fractals /random tree.py
+++ general data structures/Fractals /random tree.py
@@ -7,11 +7,11 @@ def rgb_to_hex(r, g, b):
     Converts RGB color values (0-255) to a hexadecimal string.
     """
     return "#{:02X}{:02X}{:02X}".format(int(r), int(g), int(b))
-def randomTree(r,len,size,x,y,dir,first=False):
+def randomTree(r,len,size,x,y,dir,first=False, perfect=False):
     if r <= 0:
         return
     randomNumThing = ((math.pi/10)*2)*random.random()-(math.pi/10)
-    if first:
+    if first or perfect:
         randomNumThing = 0
     nextPoint = Point(
         x + (len * math.sin(dir + randomNumThing)),
@@ -19,16 +19,16 @@ def randomTree(r,len,size,x,y,dir,first=False):
     )
     Line(Point(x, y), nextPoint)._draw(win, {"width": size,"fill":"black"})
 
-    randomTree(r - 1, len / 1.5, size / 1.5, nextPoint.x, nextPoint.y, dir - (math.pi / 4))
-    randomTree(r - 1, len / 1.5, size / 1.5, nextPoint.x, nextPoint.y, dir)
-    randomTree(r - 1, len / 1.5, size / 1.5, nextPoint.x, nextPoint.y, dir + (math.pi / 4))
+    randomTree(r - 1, len / 1.5, size / 1.5, nextPoint.x, nextPoint.y, dir - (math.pi / 4),perfect=perfect)
+    randomTree(r - 1, len / 1.5, size / 1.5, nextPoint.x, nextPoint.y, dir,perfect=perfect)
+    randomTree(r - 1, len / 1.5, size / 1.5, nextPoint.x, nextPoint.y, dir + (math.pi / 4),perfect=perfect)
 
 
 win = GraphWin("My Circle", 1000, 1000)  # Creates a window 1000 x 800 pixels
 win.setBackground("white")
 
 
-randomTree(20,-200,20,500,800,0,True)
+randomTree(10,-200,20,500,800,0,True,False)
 
 win.getMouse()  # Pause to view result
 win.close()  # Close window when done
